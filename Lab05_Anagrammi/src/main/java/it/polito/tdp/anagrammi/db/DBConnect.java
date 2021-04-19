93b65175431639eb56586762fbe924ca40eeadc8
Manuel Taglieri <s257476@studenti.polito.it>
2021-04-19 09:18:42 +0200

    Implementato Connection Pooling

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
@@ -1,15 +1,26 @@
 package it.polito.tdp.anagrammi.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DBConnect {
 	
+		static private HikariDataSource ds = null;
+		static private final String jdbcURL = "jdbc:mysql://localhost/dizionario?user=root&password=TdP21";
+	
 	public static Connection getConnection() throws SQLException {
 		
-		String jdbcURL = "jdbc:mysql://localhost/dizionario?user=root&password=TdP21";
-		return DriverManager.getConnection(jdbcURL);
+		if (ds==null) {
+			ds = new HikariDataSource();
+			ds.setJdbcUrl(jdbcURL);
+			// ds.setUsername ds.setPassword (se non lo faccio nella URL)
+		}
+		
+		//Non serve chiudere il dataSource, si chiude quando chiuderò il programma
+		
+		return ds.getConnection();
 		
 	}
 
